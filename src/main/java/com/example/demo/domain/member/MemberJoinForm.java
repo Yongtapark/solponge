2d1f685093ac250d9bcc74082f9e15ec6afe9720
Kim Jae Hyun <84066112+takingtheback@users.noreply.github.com>
2023-04-08 15:04:30 +0900

    main, 수정필요

---
+++ src/main/java/com/example/demo/domain/member/MemberJoinForm.java
@@ -0,0 +1,72 @@
+package com.example.demo.domain.member;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+@Data
+@Entity
+public class MemberJoinForm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private Long memberNo;
+    @Enumerated(EnumType.STRING)
+    private Grade memberGrade = Grade.BASIC;
+    @NotEmpty
+    private String memberId;
+    @NotEmpty
+    private String memberPwd;
+    @NotEmpty
+    @Transient
+    private String memberPwdCheck;
+    @NotEmpty
+    private String memberName;
+    /**
+     * 주소
+     */
+
+    private String memberAddress;
+    @Transient
+    private String memberAddress1;
+    @Transient
+    private String memberAddress2;
+    @Transient
+    private String memberAddress3;
+    /**
+     * 이메일
+     */
+
+    private String memberEmail;
+    @Transient
+    @NotEmpty
+    private String memberEmail1;
+    @Transient
+    @NotEmpty
+    private String memberEmail2;
+    /**
+     * 휴대폰
+     */
+    private String memberPhone;
+    @Transient
+    @NotEmpty
+    private String memberPhone1;
+    @Transient
+    @NotEmpty
+    private String memberPhone2;
+    @Transient
+    @NotEmpty
+    private String memberPhone3;
+
+    public MemberJoinForm() {
+    }
+
+    public MemberJoinForm(String memberId, String memberPwd, String memberName, String memberAddress, String memberEmail, String memberPhone) {
+        this.memberId = memberId;
+        this.memberPwd = memberPwd;
+        this.memberName = memberName;
+        this.memberAddress = memberAddress;
+        this.memberEmail = memberEmail;
+        this.memberPhone = memberPhone;
+    }
+}
